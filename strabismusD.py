137017f92e0c553d5a8727ad96caf17bb9394470
Roque A Ramírez <109245164+Armando8bits@users.noreply.github.com>
2025-03-04 20:07:08 -0500

    se divide logica de detección ajustando cierre de ventana

--- strabismusD.py
+++ strabismusD.py
@@ -4,31 +4,23 @@ import time
 import os
 
 def detectar_desviacion(face_landmarks):
-    """
-    Detecta la desviación de los ojos a partir de los puntos de referencia faciales.
-    
-    Args:
-        face_landmarks: Los puntos de referencia faciales detectados por MediaPipe.
-        
-    Returns:
-        True si se detecta desviación, False en caso contrario.
-    """
     altura_ojos = [face_landmarks.landmark[i].y for i in [159, 145, 386, 374]]
     ojo_izq_centro = (altura_ojos[0] + altura_ojos[1]) / 2
     ojo_der_centro = (altura_ojos[2] + altura_ojos[3]) / 2
-    return abs(ojo_izq_centro - ojo_der_centro) > 0.02  # Umbral para detectar desviación
+    return abs(ojo_izq_centro - ojo_der_centro) > 0.02
 
 def main():
     mp_face_mesh = mp.solutions.face_mesh
     face_mesh = mp_face_mesh.FaceMesh()
     cap = cv2.VideoCapture(0)
     
-    # Crear carpeta con la fecha actual
     fecha_actual = time.strftime("%Y%m%d")
     carpeta_destino = os.path.join(os.getcwd(), f"estrabismo_{fecha_actual}")
     os.makedirs(carpeta_destino, exist_ok=True)
     
-    while cap.isOpened():
+    cv2.namedWindow("Detección de Estrabismo")  # Crear una ventana con nombre
+    
+    while True:  # Bucle infinito
         ret, frame = cap.read()
         if not ret:
             break
@@ -46,8 +38,10 @@ def main():
                     print(f"Desviación detectada, imagen guardada como {filename}")
                     
         cv2.imshow("Detección de Estrabismo", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        
+        k = cv2.waitKey(1)
+        if k == ord('q') or cv2.getWindowProperty("Detección de Estrabismo", cv2.WND_PROP_VISIBLE) < 1:
+            break  # Salir si se presiona 'q' o se cierra la ventana
     
     cap.release()
     cv2.destroyAllWindows()
