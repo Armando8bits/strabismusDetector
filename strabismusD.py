80383883b9330a23eafce6c2ce47dc93cb9e688f
Roque A Ramírez <109245164+Armando8bits@users.noreply.github.com>
2025-03-04 20:33:41 -0500

    se ajusta para detectar parpadeos evitando falsos positivos

--- strabismusD.py
+++ strabismusD.py
@@ -3,11 +3,21 @@ import mediapipe as mp
 import time
 import os
 
+nombreAPP = "Detector de Estrabismo by Armando8bits"
+
 def detectar_desviacion(face_landmarks):
     altura_ojos = [face_landmarks.landmark[i].y for i in [159, 145, 386, 374]]
     ojo_izq_centro = (altura_ojos[0] + altura_ojos[1]) / 2
     ojo_der_centro = (altura_ojos[2] + altura_ojos[3]) / 2
-    return abs(ojo_izq_centro - ojo_der_centro) > 0.02
+    return abs(ojo_izq_centro - ojo_der_centro) > 0.02 #el umbral de desviación debe ser ajusado según velocidad de cámara y luminosidad.
+
+def detectar_parpadeo(face_landmarks):
+    # Distancia vertical entre los párpados
+    distancia_ojo_izq = abs(face_landmarks.landmark[159].y - face_landmarks.landmark[145].y)
+    distancia_ojo_der = abs(face_landmarks.landmark[386].y - face_landmarks.landmark[374].y)
+    # Umbral para detectar parpadeo (ajusta según sea necesario)
+    umbral_parpadeo = 0.005
+    return distancia_ojo_izq < umbral_parpadeo or distancia_ojo_der < umbral_parpadeo
 
 def main():
     mp_face_mesh = mp.solutions.face_mesh
@@ -18,7 +28,7 @@ def main():
     carpeta_destino = os.path.join(os.getcwd(), f"estrabismo_{fecha_actual}")
     os.makedirs(carpeta_destino, exist_ok=True)
     
-    cv2.namedWindow("Detección de Estrabismo")  # Crear una ventana con nombre
+    cv2.namedWindow(nombreAPP)  # Crear una ventana con nombre
     
     while True:  # Bucle infinito
         ret, frame = cap.read()
@@ -31,16 +41,16 @@ def main():
         
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                if detectar_desviacion(face_landmarks):
+                if not detectar_parpadeo(face_landmarks) and detectar_desviacion(face_landmarks):
                     timestamp = time.strftime("%H%M%S")
                     filename = os.path.join(carpeta_destino, f"estrabismo_{timestamp}.png")
                     cv2.imwrite(filename, frame)
                     print(f"Desviación detectada, imagen guardada como {filename}")
                     
-        cv2.imshow("Detección de Estrabismo", frame)
+        cv2.imshow(nombreAPP, frame)
         
         k = cv2.waitKey(1)
-        if k == ord('q') or cv2.getWindowProperty("Detección de Estrabismo", cv2.WND_PROP_VISIBLE) < 1:
+        if k == ord('q') or cv2.getWindowProperty(nombreAPP, cv2.WND_PROP_VISIBLE) < 1:
             break  # Salir si se presiona 'q' o se cierra la ventana
     
     cap.release()
